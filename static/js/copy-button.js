6dcd5459b4d0e8de16460bc129bb4e71bb2a0fe6
Nasser Boukehil <boukehil.nasreddine@gmail.com>
2023-04-17 17:33:54 +0200

    cleaned style and js

--- static/js/copy-button.js
+++ static/js/copy-button.js
@@ -1,10 +1,8 @@
 function copyToClipboard(button) {
-    console.log('%cCOPY-SCRIPT', 'color:orange');
     const codeBlock = button.parentNode.parentNode.nextSibling;
     const codeText = codeBlock.innerText || codeBlock.textContent;
     navigator.clipboard.writeText(codeText).then(function() {
         const feedback = button.nextSibling.nextSibling;
-        console.log('%cFEED-BACK', 'color:magenta', feedback);
         feedback.style.display = 'block';
         setTimeout(function() {
             feedback.style.display = 'none';
